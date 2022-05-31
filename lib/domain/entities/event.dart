72b9dc5a6352573e3a5926fd28e1cb3f7a7b73f9
Alejandro-Huaman <90795926+Alejandro-Huaman@users.noreply.github.com>
2022-05-31 18:45:15 -0500

    Commit EventListView design done

--- lib/domain/entities/event.dart
+++ lib/domain/entities/event.dart
@@ -2,11 +2,15 @@ class Event{
   String eventName;
   String eventDescription;
   String date;
+  String artist;
+  int likes;
 
   Event({
     this.eventName = '',
     this.eventDescription = '',
-    this.date = ''
+    this.date = '',
+    this.artist = '',
+    this.likes = 0
   });
 
 }
--- lib/presentation/views/event/eventlistview.dart
+++ lib/presentation/views/event/eventlistview.dart
@@ -13,10 +13,10 @@ class EventListView extends StatefulWidget {
 
 class _EventListViewState extends State<EventListView> {
   List<Event> events = [
-    Event(eventName: 'Event 1',eventDescription: 'a description 1',date: '30/05/2022'),
-    Event(eventName: 'Event 2',eventDescription: 'a description 2',date: '31/05/2022'),
-    Event(eventName: 'Event 3',eventDescription: 'a description 3',date: '25/05/2022'),
-    Event(eventName: 'Event 4',eventDescription: 'a description 4',date: '20/05/2022')
+    Event(eventName: 'Event 1',eventDescription: 'a description 1',date: '30/05/2022',artist: 'Artist 1',likes: 0),
+    Event(eventName: 'Event 2',eventDescription: 'a description 2',date: '31/05/2022',artist: 'Artist 2',likes: 5),
+    Event(eventName: 'Event 3',eventDescription: 'a description 3',date: '25/05/2022',artist: 'Artist 3',likes: 2),
+    Event(eventName: 'Event 4',eventDescription: 'a description 4',date: '20/05/2022',artist: 'Artist 4',likes: 3)
   ];
 
   @override
@@ -34,13 +34,43 @@ class _EventListViewState extends State<EventListView> {
               padding: EdgeInsets.all(10),
               child: Card(
                   child: ListTile(
-                    onTap: (){},
-                    title: Text(events[index].eventName),
+                    title: Row(
+                        children: <Widget>[
+                          Text(events[index].artist),
+                          SizedBox(width: 70,),
+                          Text(events[index].eventName)
+                        ],
+                    ),
                     subtitle: Column(
                         children: <Widget>[
+                          SizedBox(height: 10,),
                           Text(events[index].eventDescription),
-                          SizedBox(height: 5,),
-                          Text(events[index].date)
+                          SizedBox(height: 10,),
+                          Row(
+                            children: <Widget>[
+                              Text(events[index].date),
+                              IconButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      events[index].likes += 1;
+                                    });
+                                    print('event ${index} number of likes: ${events[index].likes}');
+                                  },
+                                  icon: Icon(Icons.thumb_up)
+                              ),
+                              IconButton(
+                                  onPressed: (){
+                                    setState(() {
+                                      events[index].likes -= 1;
+                                    });
+                                    print('event ${index} number of likes: ${events[index].likes}');
+                                  },
+                                  icon: Icon(Icons.thumb_down)
+                              ),
+                              SizedBox(width: 20,),
+                              Text('${events[index].likes}'),
+                            ],
+                          )
                         ],
                     ),
                     isThreeLine: true,
